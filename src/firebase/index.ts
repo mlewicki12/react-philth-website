d64f7c5dae9146aa48aacfe55df36c3a1cdec0bf
Maciej Lewicki <lewickimapl@gmail.com>
2021-07-28 13:26:26 +0200

    bring back secret variables

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,8 +7,8 @@ import { createContext, useContext } from 'react';
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
@@ -19,7 +19,6 @@ class Firebase {
   provider: firebase.auth.GoogleAuthProvider;
 
   constructor() {
-    console.log('initialising firebase with config: ', config);
     firebase.initializeApp(config);
 
     this.auth = firebase.auth();
